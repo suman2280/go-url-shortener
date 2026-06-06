a450d8a928cc2129e7751c7c89f1427b385ed313
Suman Sardar <sudarshansardar22@gmail.com>
2026-06-06 11:36:50 +0530

    initial setup

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+    r.GET("/", func(ctx *gin.Context) {
+        ctx.JSON(200, gin.H{
+            "message": "Hey Go URL Shortener!",
+        })
+    })
+
+    err := r.Run(":8080")
+    if err != nil {
+        panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
+    }
+}
